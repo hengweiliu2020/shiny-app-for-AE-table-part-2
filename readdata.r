d9988dfc7f95e9faeb62a53cbc1fc136f69d768a
hengweiliu2020 <73909735+hengweiliu2020@users.noreply.github.com>
2022-04-24 19:34:08 -0400

    Add files via upload

---
+++ readdata.r
@@ -0,0 +1,3 @@
+library(haven)
+adsl <- read_sas("C:\\Users\\hengw\\OneDrive\\Documents\\ae\\part2\\adsl.sas7bdat")
+adae <- read_sas("C:\\Users\\hengw\\OneDrive\\Documents\\ae\\part2\\adae.sas7bdat")
